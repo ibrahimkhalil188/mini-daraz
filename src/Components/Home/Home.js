d1507069827b535bd62c04e63dcd49969b1f93ee
unknown <ibrahimkhalil.mine@gmail.com>
2022-04-01 08:17:42 +0600

    loaded done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import useProduct from '../CustomHook/FetchHook';
+import Products from '../Products/Products';
+import "./Home.css"
 
 
 const Home = () => {
     const [products] = useProduct()
     return (
-        <div>
-            <h1>This is home page {products.length}</h1>
+        <div className='home ml-8 mt-12'>
+            <div className='grid grid-cols-3 gap-4'>
+                {
+                    products.map(product => <Products key={product.id} product={product} ></Products>)
+                }
+            </div>
+            <div>
+                <h1>This is Cart</h1>
+            </div>
+
         </div>
     );
 };
---
+++ src/Components/Products/Products.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Products = ({ product }) => {
+    const { name, img, price, seller } = product
+    return (
+        <div className=' border-2 shadow-xl p-3 rounded'>
+            <img className='rounded' src={img} alt="" />
+            <div className='mt-4 relative h-[200px]'>
+                <h1 className='text-2xl font-bold' title={name}>{name.length > 20 ? name.slice(0, 20) + "..." : name}</h1>
+                <p className='text-2xl font-bold font-mono'>Price: ${price}</p>
+                <p className='text-2xl'>Brand: {seller}</p>
+                <button className='py-3 px-3 text-white rounded-xl w-full absolute bottom-0 text-bold text-2xl bg-cyan-400 hover:text-orange-500'>Add to Cart</button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Products;
